8c89d114bd2b318360e7da0d729428b9359fa8f8
enokai <oples.treasure@gmail.com>
2023-12-07 00:26:06 +0100

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import PlayerBar from "./components/PlayerBar.svelte";
+    import Greet from "./lib/Greet.svelte";
+    import TailwindCss from "./lib/TailwindCss.svelte";
+    import "./styles.css";
+</script>
+
+<TailwindCss />
+
+<main class="bg-gray-950 w-full h-full flex flex-col">
+    <div class="flex-1">
+        <h1 class="">Welcome to Tauri!</h1>
+        <Greet />
+    </div>
+    <PlayerBar />
+</main>
+
+<style>
+</style>
---
+++ src/components/PlayerBar.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+    import { Pause, Play } from "lucide-svelte";
+
+    let isPlaying = false;
+    let volume = 50;
+
+    function togglePlay() {
+        isPlaying = !isPlaying;
+    }
+
+    function changeVolume(event: any) {
+        volume = event.target.value;
+    }
+</script>
+
+<div class="media-player">
+    <div class="controls">
+        <button class="play-pause-btn" on:click={togglePlay}>
+            {#if isPlaying}
+                <Pause />
+            {:else}
+                <Play />
+            {/if}
+        </button>
+        <input
+            type="range"
+            class="volume-slider"
+            min="0"
+            max="100"
+            bind:value={volume}
+            on:input={changeVolume}
+        />
+    </div>
+</div>
+
+<style>
+    .media-player {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 10px;
+        background-color: #333;
+        color: white;
+    }
+
+    .controls {
+        @apply flex flex-1 items-center;
+    }
+
+    .play-pause-btn {
+        margin-right: 10px;
+        cursor: pointer;
+    }
+
+    .volume-slider {
+        @apply flex-1;
+    }
+</style>
